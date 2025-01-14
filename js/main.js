92548109e53d0209edfcac74b18fa19a4b56bb51
ValentinNov <95715881+ValentinNov@users.noreply.github.com>
2025-01-14 16:54:23 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    $('.slider').slick({
+        // dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev.svg" alt="arrow-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt="arrow-next"></button>',
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+    });
+
+
+
+});
